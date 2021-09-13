ba77c29c8f8e960e1a9c2559b0c912876019f5d5
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-09-13 08:59:52 +0200

    Initial commit

---
+++ covidApp.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 17:35:03 2021
+
+@author: Anja
+"""
+
+import pandas as pd
+from shapely.geometry import polygon
+from shapely.geometry import MultiPolygon
+import geopandas as gpd
+from datetime import timedelta
+from load_data import get_CovidData
+from help_timer_functions import *
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+import plotly.express as px
+
+
+data, bezirksgrenzen, df = get_CovidData()
+
+bezirks = ['Mitte', 'Friedrichshain-Kreuzberg', 'Pankow',
+           'Charlottenburg-Wilmersdorf', 'Spandau', 'Steglitz-Zehlendorf',
+           'Tempelhof-Schöneberg', 'Neukölln', 'Treptow-Köpenick',
+           'Marzahn-Hellersdorf', 'Lichtenberg', 'Reinickendorf']
+
+
+
+
+# The initial figure
+time = data.Datum.max()
+data_datum_x = data[data.Datum==time]
+df = pd.DataFrame({'Bezirk': bezirks,
+                   '7dI': [float(data_datum_x[bez+'_7dI']) for bez in bezirks]})
+fig = px.choropleth(#_mapbox(
+    df, geojson=bezirksgrenzen, color='7dI',
+    color_continuous_scale="Viridis",
+    locations="Bezirk", #mapbox_style="carto-positron",
+    #zoom=9, center = {"lat": 52.5200, "lon": 13.4050},
+    #range_color=(0, 80), opacity=0.5)
+    labels={'7dI':'7 day incidence rate'})
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(
+    margin={"r": 0, "t": 0, "l": 0, "b": 0},)
+
+fig_total_In = px.line(data, x='Datum',
+                       y='All_berlin_7dI',
+                       title='Total 7-day incidence of Berlin')
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.P("Covid 7day incidence in Berlin - timeline"),
+    dcc.Graph(id="choropleth", figure=fig),
+    dcc.Slider(id='timeline',
+               min=unixTimeMillis(data.Datum.min()),
+               max=unixTimeMillis(data.Datum.max()),
+               value=unixTimeMillis(data.Datum.max()),
+               marks=getMarks(data.Datum.min(),
+                           data.Datum.max(),10),
+    ),
+    dcc.Graph(id='total_7dIn', figure=fig_total_In)
+    
+])
+
+@app.callback(
+    [Output("choropleth", "figure"),
+     Output('total_7dIn', 'figure')],
+    [Input("timeline", "value")],
+    [State('choropleth', 'relayoutData'),
+     State('choropleth', 'figure')]
+)
+
+def display_choropleth(time, relayoutData, figure):
+    
+    time = unixToDatetime(time)
+    data_datum_x = data[data.Datum==time]
+    z_new = [float(data_datum_x[bez+'_7dI']) for bez in bezirks]
+    figure['data'][0]['z'] = z_new
+    fig = figure
+
+    fig_total_In = px.line(data,
+                           x='Datum',
+                           y='All_berlin_7dI',
+                           title='Total 7-day incidence of Berlin')
+    fig_total_In.update_layout(shapes=[
+        dict(
+          type='line',
+          yref='paper', y0=0, y1=1,
+          xref='x', x0=time, x1=time
+        )
+    ])
+    return fig, fig_total_In
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
---
+++ help_timer_functions.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 12 17:59:04 2021
+
+@author: Anja
+"""
+
+import time 
+import unittest
+from datetime import timedelta
+import pandas as pd
+
+def unixTimeMillis(dt):
+    ''' Convert datetime to unix timestamp '''
+    return int(time.mktime(dt.timetuple()))
+
+
+def unixToDatetime(unix):
+    ''' Convert unix timestamp to datetime. '''
+    return pd.to_datetime(unix, unit='s', origin='unix').round('1d')
+
+def getMarks(start, end, N=20):
+    ''' 
+    start: Time in datetime format
+    end: time in datetime format
+    N: how many marks
+    
+    Returns the marks for labeling. 
+    Every Nth value will be used.
+    '''
+    start_unix = unixTimeMillis(start)
+    end_unix = unixTimeMillis(end)
+
+    result = {date:str(unixToDatetime(date))[:10] for date in range(start_unix,end_unix,int((end_unix-start_unix)/N))}
+
+    return result
+
+class TestDateConversion(unittest.TestCase):
+    
+    def test_unixConversion(self):
+        g = pd.to_datetime('2021-06-23', format="%Y-%m-%d")
+        self.assertEqual(g, unixToDatetime(unixTimeMillis(g)))
+
+    def test_unixConversionHours(self):
+        g = pd.to_datetime('2021-06-23', format="%Y-%m-%d")
+        h = g+timedelta(hours=2)
+        self.assertEqual(g, unixToDatetime(unixTimeMillis(h)))
+
+
+    def test_getMarks_len(self):
+        end = pd.to_datetime('2021-06-23', format="%Y-%m-%d")
+        start = pd.to_datetime('2020-06-23', format="%Y-%m-%d")
+        self.assertEqual(20, len(getMarks(start, end, 20)))
+
+if __name__ == '__main__':
+    unittest.main(argv=[''], verbosity=1, exit=False)
